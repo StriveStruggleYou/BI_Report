0403552f60f62ab8537eea671f3e4a05d40663b7
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-21 20:55:43 +0800

    提交基础数据

---
+++ server_echarts/src/main/java/org/ssy/echarts/core/Result.java
@@ -0,0 +1,43 @@
+package org.ssy.echarts.core;
+
+import com.alibaba.fastjson.JSON;
+
+public class Result {
+
+  private int code;
+  private String message;
+  private Object data;
+
+  public Result setCode(Integer resultCode) {
+    this.code = resultCode;
+    return this;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public Result setMessage(String message) {
+    this.message = message;
+    return this;
+  }
+
+  public Object getData() {
+    return data;
+  }
+
+  public Result setData(Object data) {
+    this.data = data;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return JSON.toJSONString(this);
+  }
+}
+
